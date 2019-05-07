de8cf9450032b16c00e2860db07320884b2cf7dd
Sayed Abdul Azeem Zahed <eng.sazeem@gmail.com>
2019-05-07 09:27:04 +0430

    Plane,Address

--- NHCM.Application/Recruitment/Commands/SavePersonAddressCommand.cs
+++ NHCM.Application/Recruitment/Commands/SavePersonAddressCommand.cs
@@ -5,6 +5,7 @@ using NHCM.Application.Recruitment.Models;
 using NHCM.Application.Recruitment.Queries;
 using NHCM.Domain.Entities;
 using NHCM.Persistence;
+using NHCM.Persistence.Infrastructure.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,10 +46,12 @@ namespace NHCM.Application.Recruitment.Commands
 
         private HCMContext _context;
         private IMediator _mediator;
-        public SavePersonAddressCommandHandler(HCMContext context, IMediator mediator)
+        private readonly ICurrentUser _currentUser;
+        public SavePersonAddressCommandHandler(HCMContext context, IMediator mediator, ICurrentUser currentUser)
         {
             _context = context;
             _mediator = mediator;
+            _currentUser = currentUser;
         }
         public async Task<List<SearchedPersonAdress>> Handle(SavePersonAddressCommand request, CancellationToken cancellationToken)
         {
@@ -56,45 +59,59 @@ namespace NHCM.Application.Recruitment.Commands
 
             if(request.Id == null || request.Id == default(decimal))
             {
+                int CurrentUserId = await _currentUser.GetUserId();
 
-
-                using (_context)
+                using (var transaction = _context.Database.BeginTransaction())
                 {
-                    Address personAddress = new Address()
+                    try
                     {
 
-                        PersonId = request.PersonId,
-                        ModifiedOn = request.ModifiedOn,
-                        ModifiedBy = request.ModifiedBy,
-                        ReferenceNo = request.ReferenceNo,
-                        CreatedOn = request.CreatedOn ,
-                        CreatedBy = request.CreatedBy ,
-                        AddressTypeId = request.AddressTypeId ?? 1, // Untill new Database schema
-                        LocationId = request.LocationId,
-                        DistrictId = request.DistrictId,
-                        Village = request.Village,
-                        Address1 = request.Address1,
-                        StreetNo = request.StreetNo,
-                        HouseNo = request.HouseNo,
-                        Phone = request.Phone,
-                        EmailAddress = request.EmailAddress,
-                        Mobile = request.Mobile,
-                        IsActive = request.IsActive,
-                        Paddress = request.Paddress,
-                        ClocationId = request.ClocationId,
-                        CdistrictId = request.CdistrictId,
-                        Cvillage = request.Cvillage,
-                    };
+                        using (_context)
+                        {
+                            Address personAddress = new Address()
+                            {
 
-                    _context.Address.Add(personAddress);
-                    await _context.SaveChangesAsync(cancellationToken);
+                                PersonId = request.PersonId,
+                                ModifiedOn = request.ModifiedOn,
+                                ModifiedBy = request.ModifiedBy,
+                                ReferenceNo = request.ReferenceNo,
+                                CreatedOn = request.CreatedOn,
+                                CreatedBy = request.CreatedBy,
+                                AddressTypeId = request.AddressTypeId ?? 1, // Untill new Database schema
+                                LocationId = request.LocationId,
+                                DistrictId = request.DistrictId,
+                                Village = request.Village,
+                                Address1 = request.Address1,
+                                StreetNo = request.StreetNo,
+                                HouseNo = request.HouseNo,
+                                Phone = request.Phone,
+                                EmailAddress = request.EmailAddress,
+                                Mobile = request.Mobile,
+                                IsActive = request.IsActive,
+                                Paddress = request.Paddress,
+                                ClocationId = request.ClocationId,
+                                CdistrictId = request.CdistrictId,
+                                Cvillage = request.Cvillage,
+                            };
+
+                            _context.Address.Add(personAddress);
+                            await _context.SaveChangesAsync(CurrentUserId, cancellationToken);
 
 
-                    result = await _mediator.Send(new SearchPersonAddressQuery() { Id = personAddress.Id });
-                }
+                            result = await _mediator.Send(new SearchPersonAddressQuery() { Id = personAddress.Id });
+
+                            transaction.Commit();
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw new Exception();
+                    } 
+                } 
                 
-            }
-
+            } 
             else
             {
 
